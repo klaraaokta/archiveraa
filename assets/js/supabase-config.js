f0d1f2ba10c17dbfc6f5964b4b1895fc73158895
klaraaokta <cokta311@gmail.com>
2026-09-05 17:48:51 +0700

    Ini adallah archive saya

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,8 +1,8 @@
 // ===========================================================
 // SUPABASE CONFIG
 // ===========================================================
-window.SUPABASE_URL = "https://hpzikpkwrtimnbardwhx.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_MnnK9-rm1Rpr5OohcXHhxA_ZmAchFdt";
+window.SUPABASE_URL = "https://jryvrvkgzgvkbzjefcrk.supabase.co/rest/v1/";
+window.SUPABASE_ANON_KEY = "sb_publishable_PE4dyaT6V4NN7rM1gz6fhA_IxzJhmOr";
 
 window.supabaseClient = window.supabase.createClient(
   window.SUPABASE_URL,
